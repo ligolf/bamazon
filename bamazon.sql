5b48fe7f7757beffa5dca5cef9f3127c5d009cef
gilllian ginsberg <gillian.ginsberg@gmail.com>
2019-06-13 22:51:53 -0700

    trying to get this to work

---
+++ bamazon.sql
@@ -0,0 +1,18 @@
+DROP DATABASE IF EXISTS bamazon;
+CREATE database bamazon;
+
+USE bamazon;
+
+CREATE TABLE products
+(
+    item_id INT NOT NULL,
+    product_name VARCHAR(100) NULL,
+    department_name VARCHAR(100) NULL,
+    price DECIMAL(10,4) NULL,
+    stock_quantity int(4) NULL,
+    PRIMARY KEY (item_id)
+);
+
+SELECT *
+FROM products;
+
